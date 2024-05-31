7dd8e2e7036a199c7bf1ed7827e01a5a194f377a
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-05-31 19:55:42 +0000

    conversionOfDataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33ravi"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumnber = Number(score)
+//console.log(typeof valueInNumnber);
+//console.log(valueInNumnber);
+
+
+//"33" => 33
+//"33ravi" => Nan
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
